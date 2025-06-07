60a9f92b840a6e7a502d3c5304e2901d12327c01
Dipendra <hehesuiii69@gmail.com>
2025-06-07 17:11:42 +0545

    routes ,models and middlewares added

---
+++ routes/admin/adminUserRoute.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const router = express.Router()
+const {createUser, getUsers, 
+    getOneUser, updateOneUser,
+    deleteOneUser} = require("../../controllers/admin/usermanagement")
+const { authenticateUser, isAdmin } = require("../../middlewares/authorizedUser")
+
+router.post(
+    "/create",
+    createUser
+)
+
+router.get(
+    "/",
+    authenticateUser,
+    isAdmin,
+    getUsers
+)
+
+router.get(
+    "/:id", // req.params.id
+    getOneUser
+)
+
+router.put(
+    "/:id", // req.params.id
+    updateOneUser
+)
+
+router.delete(
+    "/:id", // req.params.id
+    deleteOneUser
+)
+
+module.exports = router
+// This route handles user management for admin users
+// It includes routes for creating, reading, updating, and deleting users
