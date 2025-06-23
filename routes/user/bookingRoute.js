76e55dc2cf1287d35a1e59dcf05c0ca2f189a739
Dipendra <hehesuiii69@gmail.com>
2025-06-23 12:11:43 +0545

    made user to edit the booking and delet them

--- routes/user/bookingRoute.js
+++ routes/user/bookingRoute.js
@@ -1,26 +1,31 @@
 // const express = require('express');
 // const router = express.Router();
 
-// const { getUserBookings, createBooking } = require('../../controllers/user/bookingController');
+// const { getUserBookings, createBooking, confirmPayment } = require('../../controllers/user/bookingController');
 // const { authenticateUser } = require('../../middlewares/authorizedUser');
 
 // router.route('/bookings')
 //     .get(authenticateUser, getUserBookings)
 //     .post(authenticateUser, createBooking);
 
+// router.route('/bookings/:id/pay')
+//     .put(authenticateUser, confirmPayment);
+
 // module.exports = router;
-// // This code sets up routes for user bookings.
-// // It includes a GET route to fetch user bookings and a POST route to create a new booking
 
 const express = require('express');
 const router = express.Router();
 
-const { getUserBookings, createBooking, confirmPayment } = require('../../controllers/user/bookingController');
+const { getUserBookings, createBooking, updateUserBooking, deleteUserBooking, confirmPayment } = require('../../controllers/user/bookingController');
 const { authenticateUser } = require('../../middlewares/authorizedUser');
 
 router.route('/bookings')
     .get(authenticateUser, getUserBookings)
     .post(authenticateUser, createBooking);
+
+router.route('/bookings/:id')
+    .put(authenticateUser, updateUserBooking)
+    .delete(authenticateUser, deleteUserBooking);
 
 router.route('/bookings/:id/pay')
     .put(authenticateUser, confirmPayment);
