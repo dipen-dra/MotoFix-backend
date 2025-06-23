042caf10fa669cf9085b2e0c50e3acf66620d5b5
Dipendra <hehesuiii69@gmail.com>
2025-06-23 20:29:47 +0545

    working on khalti as a payement error in verifing

--- routes/user/bookingRoute.js
+++ routes/user/bookingRoute.js
@@ -1,22 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-
-// const { getUserBookings, createBooking, updateUserBooking, deleteUserBooking, confirmPayment } = require('../../controllers/user/bookingController');
-// const { authenticateUser } = require('../../middlewares/authorizedUser');
-
-// router.route('/bookings')
-//     .get(authenticateUser, getUserBookings)
-//     .post(authenticateUser, createBooking);
-
-// router.route('/bookings/:id')
-//     .put(authenticateUser, updateUserBooking)
-//     .delete(authenticateUser, deleteUserBooking);
-
-// router.route('/bookings/:id/pay')
-//     .put(authenticateUser, confirmPayment);
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
 
