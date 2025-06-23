dbe5cb332551e4f4dde46c94fee1ba60e45e1f22
Dipendra <hehesuiii69@gmail.com>
2025-06-23 11:15:49 +0545

    address added and fecthed added geoloaction

--- models/User.js
+++ models/User.js
@@ -1,37 +1,3 @@
-// const mongoose = require("mongoose")
-
-// const Schema = mongoose.Schema
-
-// const UserSchema = new Schema(
-//     {
-//         // username: {
-//         //     type: String,
-//         //     required: true,
-//         //     unique: true
-//         // },
-//         fullName: {
-//             type: String,
-//             required: true
-//         },
-//         email: {
-//             type: String,
-//             required: true,
-//             unique: true
-//         },
-//         password: {
-//             type: String,
-//             required: true
-//         },
-//         role: {
-//             type: String,
-//             default: "normal"
-//         }
-//     },
-//     { timestamps: true }
-// )
-
-// module.exports = mongoose.model("User", UserSchema)
-
 
 const mongoose = require("mongoose")
 
@@ -62,6 +28,10 @@ const UserSchema = new Schema(
             default: "normal"
         },
         phone: {
+            type: String,
+            default: ""
+        },
+        address: {
             type: String,
             default: ""
         },
--- routes/admin/bookingRoute.js
+++ routes/admin/bookingRoute.js
@@ -1,32 +1,32 @@
-// const express = require("express");
+
+
+// const express = require('express');
 // const router = express.Router();
-// const { getBookings, updateBooking, deleteBooking } = require("../../controllers/admin/bookingController");
-// const { authenticateUser, isAdmin } = require("../../middlewares/authorizedUser");
 
-// router.get("/", authenticateUser, isAdmin, getBookings);
-// router.put("/:id", authenticateUser, isAdmin, updateBooking);
-// router.delete("/:id", authenticateUser, isAdmin, deleteBooking);
+// const { 
+//     getAllBookings, 
+//     getBookingById,  
+//     deleteBooking, 
+//     updateBooking
+// } = require('../../controllers/admin/bookingController');
+// const { authenticateUser } = require('../../middlewares/authorizedUser');
 
-// module.exports = router;
+// // GET all bookings
+// // Matches /api/admin/bookings
+// router.route('/')
+//     .get(authenticateUser, getAllBookings);
 
+// // GET a single booking by ID
+// // DELETE a single booking by ID
+// // Matches /api/admin/bookings/:id
+// router.route('/:id')
+//     .get(authenticateUser, getBookingById)
+//     .delete(authenticateUser, deleteBooking);
 
-// const express = require("express");
-// const router = express.Router();
-// // --- FIX: Import the getBooking function ---
-// const { getBookings, getBooking, updateBooking, deleteBooking } = require("../../controllers/admin/bookingController");
-// const { authenticateUser, isAdmin } = require("../../middlewares/authorizedUser");
-
-// // Gets all bookings
-// router.get("/", authenticateUser, isAdmin, getBookings);
-
-// // --- FIX: Add this route for getting a single booking by ID ---
-// router.get("/:id", authenticateUser, isAdmin, getBooking);
-
-// // Updates a booking
-// router.put("/:id", authenticateUser, isAdmin, updateBooking);
-
-// // Deletes a booking
-// router.delete("/:id", authenticateUser, isAdmin, deleteBooking);
+// // PUT (update) a booking's status
+// // Matches /api/admin/bookings/:id/status
+// router.route('/:id')
+//     .put(authenticateUser, updateBooking);
 
 // module.exports = router;
 
@@ -42,21 +42,15 @@ const {
 } = require('../../controllers/admin/bookingController');
 const { authenticateUser } = require('../../middlewares/authorizedUser');
 
-// GET all bookings
 // Matches /api/admin/bookings
 router.route('/')
     .get(authenticateUser, getAllBookings);
 
-// GET a single booking by ID
-// DELETE a single booking by ID
 // Matches /api/admin/bookings/:id
+// REFACTORED: Chained all methods for the same route together
 router.route('/:id')
     .get(authenticateUser, getBookingById)
+    .put(authenticateUser, updateBooking)
     .delete(authenticateUser, deleteBooking);
-
-// PUT (update) a booking's status
-// Matches /api/admin/bookings/:id/status
-router.route('/:id')
-    .put(authenticateUser, updateBooking);
 
 module.exports = router;
