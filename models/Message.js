80147b365f26d1a629db1d64364dc79083cc81fa
dipen-dra <dipendrajr999@gmail.com>
2025-07-04 23:21:37 +0545

    web socket started

---
+++ models/Message.js
@@ -0,0 +1,26 @@
+// motofix-backend/models/Message.js
+
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  room: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
